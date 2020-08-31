338601e6c5932970abfc6e0f66dfd7d9a786e2b7
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-08-31 17:52:18 +0800

    create router component

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import {Route, Switch} from 'react-router-dom';
+import React from 'react';
+import Home from '../pages/home';
+import Login from '../pages/login';
+import Register from '../pages/Register';
+
+const Router = () => (
+    <Switch>
+        {/* Switch相当于js中的switch function */}
+        <Route exact path="/" component={Home}/>  
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        {/* Route负责匹配一个路径和渲染此路径的组件。 */}
+    </Switch>
+);
+export default Router;
+// 把Router导出，这样在App中可以用它。
