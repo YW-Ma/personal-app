aa342b995b2a651f584256599fa6654a20486942
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-09-01 20:20:08 +0800

    CSS Module practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, NavLink} from 'react-router-dom';
+import {Menu} from 'antd';
+// import 'antd/dist/antd.less' 按需加载后不需要全局引入了
 import Router from './router';
 //等同于 
 // import { default as Router } from './router';
+import styles from './App.module.less'
 
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <li><a href = "\">Home</a></li>
-        <li><a href = "\login">Login</a></li>
-        <li><a href = "\Register">Register</a></li>
-      </ul>
+      <Menu className = {styles.menu} theme = 'dark'>
+        <Menu.Item><NavLink to = "/">Home</NavLink></Menu.Item>
+        <Menu.Item><NavLink to = "/login">Login</NavLink></Menu.Item>
+        <Menu.Item><NavLink to = "/Register">Register</NavLink></Menu.Item>
+      </Menu>
       <Router></Router>
     </BrowserRouter>
   );
