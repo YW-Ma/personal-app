4213ffd7458f794a8b723d445d82090d4693c8d5
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-09-26 21:11:15 +0800

    register page

---
+++ src/pages/Register/index-reference.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { Form } from 'antd'
+import InputItem from '../../components/InputItem'
+import SubmitButton from '../../components/SubmitButton'
+import { MailOutlined, LockTwoTone } from '@ant-design/icons';
+import styles from './index.module.less'
+
+const Register = () => {
+    const handleFinish = (values) => {
+        console.log(values)  //会自动传入一个values，submit的时候调用onFinish
+    }
+    const promise = Promise;
+    const checkConfirm = (_, value) => { // (rule, value) => Promise
+        if (value && value !== form.getFieldValue('password')) { // 如果填了确认密码，但是确认密码和密码不一样。
+            return promise.reject('两次输入的密码不一致！'); // 注意，老师先检查了value是否存在！
+        }
+        return promise.resolve(); // 成功的时候 或者什么都没输入的时候
+    }
+    const [form] = Form.useForm();
+    const mainTheme = {
+        color: "#1890ff"
+    }
+    
+
+    return (
+        // 背景容器
+        <div className={styles.registerContainer}>
+            <div className={styles.register}>
+                <Form
+                    form={form}
+                    onFinish={handleFinish}
+                >
+                    {/* 邮箱 */}
+                    <InputItem
+                        name="mail"
+                        prefix={
+                            <MailOutlined style={mainTheme} />
+                        }
+                        placeholder="邮箱"
+                        size="large"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入邮箱！'
+                            },
+                            {
+                                type: 'email', // 校验邮箱格式
+                                message: '请填写正确的邮箱格式',
+                            }
+                        ]}
+                    />
+                    {/* 密码 */}
+                    <InputItem
+                        name="password"
+                        prefix={
+                            <LockTwoTone style={mainTheme} />
+                        }
+                        placeholder="至少六位密码，区分大小写"
+                        size="large"
+                        type="password"
+                        rules={[{
+                            required: true,
+                            message: '请设置密码！'
+                        }]}
+                    />
+                    {/* 确认密码： 注意要实现和密码的联动 */}
+                    <InputItem
+                        name="confirm"
+                        prefix={
+                            <LockTwoTone style={mainTheme} />
+                        }
+                        placeholder="确认密码"
+                        size="large"
+                        type="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入确认密码！'
+                            },
+                            {
+                                validator: checkConfirm,
+                            }
+                        ]}
+                    />
+                </Form>
+            </div>
+        </div>
+    )
+};
+
+export default Register;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,64 +1,247 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import { Tabs, Form, Checkbox, Row } from 'antd'
-import InputItem from '../../components/InputItem'
-import SubmitButton from '../../components/SubmitButton'
-import { MailOutlined, LockTwoTone } from '@ant-design/icons';
-import styles from './index.module.less'
+import React, { useState } from 'react';
+import {
+  Form,
+  Input,
+  Tooltip,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
+const residences = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
 const Register = () => {
-    const handleFinish = (values) => {
-        //会自动传入一个values，submit的时候调用onFinish
-        console.log(values)
-    }
-    const [form] = Form.useForm();
-    const mainTheme = {
-        color: "#1890ff"
-    }
+  const [form] = Form.useForm();
 
-    return (
-        // 背景容器
-        <div className={styles.registerContainer}>
-            <div className={styles.register}>
-                <Form
-                    form={form}
-                    onFinish={handleFinish}
-                >
-                    <InputItem
-                        name="mail"
-                        prefix={
-                            <MailOutlined style={mainTheme} />
-                        }
-                        placeholder="邮箱"
-                        size="large"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入邮箱！'
-                            },
-                            {
-                                type: 'email', // 校验邮箱格式
-                                message: '请填写正确的邮箱格式',
-                            }
-                        ]}
-                    />
-                    <InputItem
-                        name="password"
-                        prefix={
-                            <LockTwoTone style={mainTheme} />
-                        }
-                        placeholder="密码"
-                        size="large"
-                        type="password"
-                        rules={[{
-                            required: true,
-                            message: '请输入密码！'
-                        }]}
-                    />
-                </Form>
-            </div>
-        </div>
-    )
-};
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(['.com', '.org', '.net'].map((domain) => `${value}${domain}`));
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ['zhejiang', 'hangzhou', 'xihu'],
+        prefix: '86',
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject('The two passwords that you entered do not match!');
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone number!',
+          },
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: '100%',
+          }}
+        />
+      </Form.Item>
+      
+      <Form.Item label="Captcha" extra="We must make sure that your are a human.">
+        <Row gutter={8}>
+          <Col span={12}>
+            <Form.Item
+              name="captcha"
+              noStyle
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the captcha you got!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Button>Get captcha</Button>
+          </Col>
+        </Row>
+      </Form.Item>
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject('Should accept agreement'),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 export default Register;
